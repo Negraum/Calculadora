ebca66faa42a80c5791eb24234ad00cb7b3fca0f
Negraum <Conradosoares_@outlook.com>
2024-10-10 23:08:09 -0300

    adicionando funcionabilidades

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 function criaCalculadora() {
   return{
-    // Atributos abaixo
+    // Atrubutos
     display: document.querySelector('.display'),
 
-
-    // Métodos abaixo
-    inicia() {
-      alert('Oi, iniciei')
+    // Métodos
+    clearDisplay() {
+      this.display.value = '';
     },
-    
+
+    inicia() {
+      this.cliqueBotoes();
+    },
+
+    cliqueBotoes() {
+      document.addEventListener('click', function(e) {
+        const el = e.target; // Oque foi clicado
+
+        if(el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if(el.classList.contains('btn-clear')) {
+          this.display.value = '';
+        }
 
 
-  }
+      }.bind(this));
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor
+    }
+  };
 }
 
-const calculadora = criaCalculadora()
+const calculadora = criaCalculadora();
+calculadora.inicia();
