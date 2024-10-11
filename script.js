6a8b0e97bc977d3af4af775ec8f984c2c4a9e2c9
Negraum <Conradosoares_@outlook.com>
2024-10-10 21:01:52 -0300

    inicio do projeto

---
+++ script.js
@@ -0,0 +1,17 @@
+function criaCalculadora() {
+  return{
+    // Atributos abaixo
+    display: document.querySelector('.display'),
+
+
+    // Métodos abaixo
+    inicia() {
+      alert('Oi, iniciei')
+    },
+    
+
+
+  }
+}
+
+const calculadora = criaCalculadora()
