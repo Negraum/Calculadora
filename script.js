6e3e43ee5dc3ea6283ca79113dff909120352a4d
Negraum <Conradosoares_@outlook.com>
2024-10-10 23:34:19 -0300

    add mais funcionabilidades

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 function criaCalculadora() {
   return{
-    // Atrubutos
+    
+    // Atributos
     display: document.querySelector('.display'),
 
     // Métodos
-    clearDisplay() {
-      this.display.value = '';
-    },
-
     inicia() {
       this.cliqueBotoes();
     },
 
+    clearDisplay() {
+      this.display.value = '';
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    realizaConta() {
+      let conta = this.display.value
+
+      try {
+        conta = eval(conta)
+
+        if(!conta) {
+          alert('Conta inválida');
+          return;
+        }
+
+        this.display.value = String(conta); // Transforma em string
+      } catch (error) {
+        alert('Conta inválida');
+        return;
+      }
+    },
+
     cliqueBotoes() {
-      document.addEventListener('click', function(e) {
+      document.addEventListener('click', e => {
         const el = e.target; // Oque foi clicado
 
         if(el.classList.contains('btn-num')) {
@@ -24,7 +47,15 @@ function criaCalculadora() {
           this.display.value = '';
         }
 
-      }.bind(this));
+        if(el.classList.contains('btn-del')) {
+          this.apagaUm()
+        }
+
+        if(el.classList.contains('btn-eq')) {
+          this.realizaConta();
+        }
+
+      });
     },
 
     btnParaDisplay(valor) {
